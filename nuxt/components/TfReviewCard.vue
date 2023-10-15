91ef580f3fac7119d16afd8c3cadb97280140241
cedney97 <c.edney@wustl.edu>
2023-10-15 09:25:37 -0500

    Looks pretty okay

--- nuxt/components/TfReviewCard.vue
+++ nuxt/components/TfReviewCard.vue
@@ -1,37 +1,20 @@
 <template>
-  <v-card>
+  <v-card class="bg-whiteSub my-2">
     <v-row>
-      <v-col
-        v-if="review.profile_photo_url"
-        cols="4"
-      >
-        <v-avatar
-        
-          size="30"
-        >
-          <img
-            :src="review.profile_photo_url"
-            alt="Author Photo"
-          > 
+      <v-col cols="12" class="d-flex flex-column align-center justify-flex-start my-2">
+        <v-avatar size="30">
+          <img :src="review.profile_photo_url" alt="Author Photo">
         </v-avatar>
+        <span class="author-name text-bold">{{ review.author_name }}</span>
+        <span class="rating">Rating: {{ review.rating }}<img height="15" width="15"
+            style="position: relative; top: 1.5px;" src="../images/star.png"></span>
+        <span class="text-caption">{{ review.relative_time_description }}</span>
       </v-col>
 
-      <v-col cols="8">
-        <span class="author-name">{{ review.author_name }}</span>
-        <span class="rating">Rating: {{ review.rating }}</span>
-      </v-col>
-    
       <v-col cols="12">
-        <v-card
-          max-height="200px"
-          class="text-wrap"
-        > 
+        <v-card max-height="200px" class="text-wrap pa-2 overflow-auto">
           {{ review.text }}
         </v-card>
-      </v-col>
-
-      <v-col cols="12">
-        {{ review.relative_time_description }}
       </v-col>
     </v-row>
   </v-card>
@@ -40,6 +23,6 @@
 <script setup lang="ts">
 import { PlaceReview } from '~~/types/Google/googlePlace.type';
 defineProps<{
-    review: PlaceReview
+  review: PlaceReview
 }>()
 </script>
--- nuxt/components/TfSearchCardDetailed.vue
+++ nuxt/components/TfSearchCardDetailed.vue
@@ -1,31 +1,33 @@
 <template>
   <v-card>
-    <h1 class="pa-2">{{ activityNumber + ". " + name }}</h1>
+    <h1 class="pa-2 text-primary">{{ activityNumber + ". " + name }}</h1>
     <v-row>
-      <v-col cols="4">
-        <v-img height="100%" cover
+      <v-col cols="12">
+        <v-img height="auto" width="100%" cover
           :src="coverPhoto ? coverPhoto : 'https://npf-prod.imgix.net/uploads/shutterstock_110306771.jpg?auto=compress%2Cformat&fit=max&q=80&w=1600'" />
       </v-col>
       <v-col cols="12">
-        <v-row no-gutters>
+        <v-row no-gutters class="px-2">
           <v-col cols="12" v-if="address">
-            <p class="text-caption">Address: {{ address }}</p>
+            <b>Address:</b>
+            <p>{{ address }}</p>
           </v-col>
 
-          <v-col cols="12" v-if="rating">
-            Rating: {{ rating }}
+          <v-col cols="12" v-if="rating" class="my-2">
+            <b>Rating:</b> {{ rating }} <img height="15" width="15" style="position: relative; top: 1.5px;"
+              src="../images/star.png">
           </v-col>
 
           <v-col cols="12" v-if="phoneNumber">
-            Phone Number: {{ phoneNumber }}
+            <b>Phone Number:</b> {{ phoneNumber }}
           </v-col>
 
-          <v-col cols="12" v-if="website">
+          <v-col cols="12" v-if="website" class="my-2">
             <a :href="website">Website</a>
           </v-col>
 
-          <v-col cols="12">
-            Reviews:
+          <v-col cols="12" class="my-2" v-if="reviews">
+            <b>Reviews:</b>
 
             <TfReviewCard v-for="(review, index) in reviews" :key="index" :review="review" />
           </v-col>
