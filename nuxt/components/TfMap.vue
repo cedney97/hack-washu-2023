d540fa858d873a7c26a4b2484aaa514ff066770a
cedney97 <c.edney@wustl.edu>
2023-10-14 20:08:13 -0500

    Added correct routing

--- nuxt/components/TfMap.vue
+++ nuxt/components/TfMap.vue
@@ -1,14 +1,9 @@
 <template>
-  <GoogleMap
-    api-key="AIzaSyAF5niDHCDCD640Vyx7Tv1u19-lUXL9jpw"
-    style="width: 100%; height: 100vh"
-    :center="center"
-    :zoom="15"
-  >
-    <Marker :options="{ position: center }" />
-  </GoogleMap>
+    <GoogleMap api-key="AIzaSyAF5niDHCDCD640Vyx7Tv1u19-lUXL9jpw" style="width: 100%; height: 100vh" :center="center"
+        :zoom="15">
+        <Marker :options="{ position: center }" />
+    </GoogleMap>
 </template>
-
 
 <script>
 import { defineComponent } from "vue";
@@ -22,26 +17,4 @@ export default defineComponent({
         return { center };
     },
 });
-</script><template> 
-    <GoogleMap       
-    api-key="AIzaSyAF5niDHCDCD640Vyx7Tv1u19-lUXL9jpw"   
-    style="width: 100%; height: 500px"
-    :center="center"       
-    :zoom="15"
-    >       
-        <Marker :options="{ position: center }" />
-    </GoogleMap>   
-</template>        
-
-<script>   
-    import { defineComponent } from "vue";   
-    import { GoogleMap, Marker } from "vue3-google-map";
-
-    export default defineComponent({       
-        components: { GoogleMap, Marker },       
-        setup() {           
-            const center = { lat: 40.689247, lng: -74.044502 };              
-            return { center };       
-        },   
-    });  
 </script>
--- nuxt/pages/explore.vue
+++ nuxt/pages/explore.vue
@@ -4,8 +4,17 @@
 </script>
 
 <template>
-    <div class="h-screen w-screen d-flex flex-column align-center pt-5 bg-whiteSub">
-        <TfTitle title="TravelFreely" />
-        <TfMap />
-    </div>
+  <div class="h-screen w-screen d-flex flex-column align-center pt-5 bg-whiteSub">
+    <v-container height="100vh" width="100%" class="pa-0 ma-0" fluid>
+      <v-row>
+        <v-col cols="4">
+          <TfSearchCardMini :place-id="'ChIJN1t_tDeuEmsRUsoyG83frY4'" />
+        </v-col>
+
+        <v-col cols="8">
+          <TfMap />
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
