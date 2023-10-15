788f09139dd934b8fdce255b4a7a82bdd87058e9
Talha Kiani <talha@ellipsistravel.com>
2023-10-15 08:32:33 -0500

    merging cade and kiani

--- nuxt/pages/explore.vue
+++ nuxt/pages/explore.vue
@@ -27,18 +27,47 @@ ids.value.push('ChIJN1t_tDeuEmsRUsoyG83frY4');
 ids.value.push('ChIJVTPokywQkFQRmtVEaUZlJRA');
 ids.value.push('ChIJAYWNSLS4QIYROwVl894CDco');
 
+const days = ref([['ChIJN1t_tDeuEmsRUsoyG83frY4'], ['ChIJVTPokywQkFQRmtVEaUZlJRA'], ['ChIJAYWNSLS4QIYROwVl894CDco']])
+
 </script>
 
 <template>
   <div class="h-screen w-screen d-flex flex-column align-center bg-whiteSub">
-    <v-container class="pa-4 h-100 overflow-hidden w-100" fluid>
-      <v-row>
-        <v-col cols="4" class="pa-3 overflow-auto h-screen w-100">
-          <TfCardList :place-ids="ids" title="Your Itinerary" />
+    <v-container
+      class="pa-4 h-100 overflow-hidden w-100"
+      fluid
+    >
+      <v-row no-gutters>
+        <v-col
+          cols="4"
+          class="pa-3 overflow-auto h-screen w-100"
+        >
+          <v-sheet
+            height="100vh"
+            class="overflow-y-auto"
+          >
+            <div
+              v-for="(day, i) in days"
+              :key="i"
+            >
+              <TfCardList
+                :place-ids="days[i]"
+                :title="`Day ${i + 1}`"
+              />
+            </div>
+          </v-sheet>
         </v-col>
 
-        <v-col cols="8" class="pa-0">
-          <TfMap />
+        <v-col
+          cols="8"
+          class="pa-0"
+        >
+          <v-card
+            height="100vh"
+            rounded
+          >
+            <TfMap />
+          </v-card>
         </v-col>
       </v-row>
     </v-container>
